5b7bb0f29992f023d84b44305369f6b97ac785e8
Cheatoid <Cheatoid@users.noreply.github.com>
2026-04-03 14:44:46 +0200

    introduce RAW_ASSETS_BASE_URL, use shorter raw link

--- src/main.ts
+++ src/main.ts
@@ -51,21 +51,22 @@ function generateDocsLink(
 	return `<a href="${url}">docs</a>`;
 }
 
+const RAW_ASSETS_BASE_URL = "https://raw.github.com/nanos-world/vscode-extension/master/assets/";
 function generateAuthorityString(authority: Authority) {
 	switch (authority) {
 		case "server":
-			return '<img src="https://github.com/nanos-world/vscode-extension/blob/master/assets/server-only.png?raw=true" height="21"> `Server Side`';
+			return `<img src="${RAW_ASSETS_BASE_URL}server-only.png" height="21"> \`Server Side\``;
 		case "client":
-			return '<img src="https://github.com/nanos-world/vscode-extension/blob/master/assets/client-only.png?raw=true" height="21"> `Client Side`';
+			return `<img src="${RAW_ASSETS_BASE_URL}client-only.png" height="21"> \`Client Side\``;
 		case "authority":
-			return '<img src="https://github.com/nanos-world/vscode-extension/blob/master/assets/authority-only.png?raw=true" height="21"> `Authority Side`';
+			return `<img src="${RAW_ASSETS_BASE_URL}authority-only.png" height="21"> \`Authority Side\``;
 		case "network-authority":
-			return '<img src="https://github.com/nanos-world/vscode-extension/blob/master/assets/network-authority.png?raw=true" height="21"> `Network Authority`';
+			return `<img src="${RAW_ASSETS_BASE_URL}network-authority.png" height="21"> \`Network Authority\``;
 		case "both-net-authority-first":
-			return '<img src="https://github.com/nanos-world/vscode-extension/blob/master/assets/both-net-authority-first.png?raw=true" height="21"> `Both Sides (Network Authority First)`';
+			return `<img src="${RAW_ASSETS_BASE_URL}both-net-authority-first.png" height="21"> \`Both Sides (Network Authority First)\``;
 		//case "both":
 		default:
-			return '<img src="https://github.com/nanos-world/vscode-extension/blob/master/assets/both.png?raw=true" height="21"> `Client/Server Side`';
+			return `<img src="${RAW_ASSETS_BASE_URL}both.png" height="21"> \`Client/Server Side\``;
 	}
 }
 
